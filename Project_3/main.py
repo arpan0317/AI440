77501c3d1bf1a047c77469f89e82e7d64dc775f6
Arpan Gupta <arpan0317@yahoo.com>
2021-04-10 18:26:31 -0400

    project 3 updated agents

--- Project_3/main.py
+++ Project_3/main.py
@@ -1,4 +1,4 @@
-from fraction import fraction
+from fractions import Fraction
 import random
 import sys
 import csv
@@ -18,24 +18,23 @@ class cell:
 
 class land:
     def __init__(self):
-        self.dim = 3;
+        self.dim = 30;
         self.FalseNegativeMap =  [ [ None for i in range(self.dim) ] for j in range(self.dim) ]
         self.map = [ [ None for i in range(self.dim) ] for j in range(self.dim) ]
         for i in range(0, self.dim):
             for j in range(0, self.dim):
                 pt = point(i,j)
                 rand = random.randint(1, 4)
-                c = cell(pt, rand, 1/9)
+                c = cell(pt, rand, float.hex(1/900))
                 if rand == 1:
-                    self.FalseNegativeMap[i][j] = 0.1
+                    self.FalseNegativeMap[i][j] = float.hex(0.1)
                 elif rand == 2:
-                    self.FalseNegativeMap[i][j] = 0.3
+                    self.FalseNegativeMap[i][j] = float.hex(0.3)
                 elif rand == 3:
-                    self.FalseNegativeMap[i][j] = 0.7
+                    self.FalseNegativeMap[i][j] = float.hex(0.7)
                 elif rand == 4:
-                    self.FalseNegativeMap[i][j] = 0.9
+                    self.FalseNegativeMap[i][j] = float.hex(0.9)
                 self.map[i][j] = c
-        self.Agent2Map =  self.map
 
         randCol = random.randint(0, self.dim-1)
         randRow = random.randint(0, self.dim-1)
@@ -50,7 +49,7 @@ class land:
     def printProb(self):
         for k in range(0, self.dim):
             for m in range(0, self.dim):
-                print(self.map[k][m].prob,' ', end = ' ')
+                print(float.fromhex(self.map[k][m].prob),' ', end = ' ')
             print()
 
     def updateProb(self, col, row):
@@ -58,53 +57,58 @@ class land:
             for j in range(0, self.dim):
                 if i == col and j == row:
                     continue
-                self.map[i][j].prob = (self.map[i][j].prob)/(self.map[col][row].prob*self.FalseNegativeMap[col][row]+(1-self.map[col][row].prob))
-        self.map[col][row].prob = (self.FalseNegativeMap[col][row]*self.map[col][row].prob)/(self.map[col][row].prob*self.FalseNegativeMap[col][row]+(1-self.map[col][row].prob))
-        self.printProb()
-        print()
+                self.map[i][j].prob = float.hex(float.fromhex(self.map[i][j].prob)/(float.fromhex(self.map[col][row].prob)*float.fromhex(self.FalseNegativeMap[col][row])+(1-float.fromhex(self.map[col][row].prob))))
+        self.map[col][row].prob = float.hex((float.fromhex(self.FalseNegativeMap[col][row])*float.fromhex(self.map[col][row].prob))/(float.fromhex(self.map[col][row].prob)*float.fromhex(self.FalseNegativeMap[col][row])+(1-float.fromhex(self.map[col][row].prob))))
+        #self.printProb()
+        #print()
 
     def searchNext(self, currCol, currRow):
         points = []
-        prob = self.map[0][0].prob
+        prob = round(float.fromhex(self.map[0][0].prob), 10)
         for i in range(0, self.dim):
             for j in range(0, self.dim):
-                if self.map[i][j].prob>prob:
+                #print("new prob, old prob: ", round(float.fromhex(self.map[i][j].prob), 10), round(prob, 10))
+                if round(float.fromhex(self.map[i][j].prob), 10)>round(prob, 10):
                     points = []
-                    prob = self.map[i][j].prob
+                    prob = float.fromhex(self.map[i][j].prob)
                     pt = (i,j)
                     points.append(pt)
-                elif self.map[i][j].prob == prob:
+                elif round(float.fromhex(self.map[i][j].prob), 10) == round(prob, 10):
                     pt = (i,j)
                     points.append(pt)
+                #print(points)
         col = points[0][0]
         row= points[0][1]
+        #print("col, row in search next: ", col, row)
         distance = abs(currCol-col)+abs(currRow-row)
 
         for point in points:
+        #    print("col, row: ", point[0], point[1])
             newDistance = abs(currCol-point[0])+abs(currRow-point[1])
             if distance>newDistance:
                 distance = newDistance
                 col = point[0]
                 row = point[1]
-
+        #print("distance in search next: ", distance)
         return col, row, distance
 
     def agent1(self):
-        self.printLand()
-        print("Target Row: ", self.target.col)
-        print("Target Col: ", self.target.row)
-        print()
+        #self.printLand()
+        #print("Target Row: ", self.target.col)
+        #print("Target Col: ", self.target.row)
+        #print()
         currCol = random.randint(0, self.dim-1)
         currRow = random.randint(0, self.dim-1)
         distance = 0
         search = 0
         while True:
             search +=1
-            print("Row: ", currCol)
-            print("Col: ", currRow)
+            #print("Row: ", currCol)
+            #print("Col: ", currRow)
             if self.map[currCol][currRow].pt.col == self.target.col and self.map[currCol][currRow].pt.row == self.target.row:
-                if random.random() > self.FalseNegativeMap[currCol][currRow]:
+                if random.random() > float.fromhex(self.FalseNegativeMap[currCol][currRow]):
                     print('type: ', self.map[currCol][currRow].state)
+                    #print("distance in agent 1: ", distance)
                     return distance+search
             self.updateProb(currCol, currRow)
             currCol, currRow, mDistance = self.searchNext(currCol, currRow)
@@ -115,16 +119,16 @@ class land:
     def Agent2Search(self, currCol, currRow):
         points = []
         print()
-        prob = self.map[0][0].prob*(1-self.FalseNegativeMap[0][0])
+        prob = round(float.fromhex(self.map[0][0].prob)*(1-float.fromhex(self.FalseNegativeMap[0][0])), 10)
         for i in range(0, self.dim):
             for j in range(0, self.dim):
-                print((self.map[i][j].prob*(1-self.FalseNegativeMap[i][j])), ' ', end = ' ')
-                if (self.map[i][j].prob*(1-self.FalseNegativeMap[i][j]))>prob:
+                print((float.fromhex(self.map[i][j].prob)*(1-float.fromhex(self.FalseNegativeMap[i][j]))), ' ', end = ' ')
+                if round((float.fromhex(self.map[i][j].prob)*(1-float.fromhex(self.FalseNegativeMap[i][j]))), 10)>round(prob, 10):
                     points = []
-                    prob = self.map[i][j].prob*(1-self.FalseNegativeMap[i][j])
+                    prob = float.fromhex(self.map[i][j].prob)*(1-float.fromhex(self.FalseNegativeMap[i][j]))
                     pt = (i,j)
                     points.append(pt)
-                elif (self.map[i][j].prob*(1-self.FalseNegativeMap[i][j])) == prob:
+                elif round((float.fromhex(self.map[i][j].prob)*(1-float.fromhex(self.FalseNegativeMap[i][j]))), 10) == round(prob, 10):
                     pt = (i,j)
                     points.append(pt)
             print()
@@ -132,12 +136,12 @@ class land:
         row= points[0][1]
         distance = abs(currCol-col)+abs(currRow-row)
 
-        for point in points:
-            newDistance = abs(currCol-point[0])+abs(currRow-point[1])
+        for pt in points:
+            newDistance = abs(currCol-pt[0])+abs(currRow-pt[1])
             if distance>newDistance:
                 distance = newDistance
-                col = point[0]
-                row = point[1]
+                col = pt[0]
+                row = pt[1]
 
         return col, row, distance
 
@@ -155,15 +159,40 @@ class land:
             print("Row: ", currCol)
             print("Col: ", currRow)
             if self.map[currCol][currRow].pt.col == self.target.col and self.map[currCol][currRow].pt.row == self.target.row:
-                if random.random() > self.FalseNegativeMap[currCol][currRow]:
+                if random.random() > float.fromhex(self.FalseNegativeMap[currCol][currRow]):
                     print('type: ', self.map[currCol][currRow].state)
                     return distance+search
             self.updateProb(currCol, currRow)
+            self.printProb()
+            print()
             currCol, currRow, mDistance = self.Agent2Search(currCol, currRow)
-
-
             distance += mDistance
 
-landTest = land()
-score = landTest.agent2()
-print('score: ', score)
+
+#scoreAgent1 = 0
+#for i in range(0, 25):
+#    print(i)
+#    landTest1 = land()
+#    score1 = landTest1.agent1()
+#    print(score1)
+#    scoreAgent1 += score1
+
+#print("Agent1: ", scoreAgent1/25)
+
+#scoreAgent2 = 0
+#for j in range(0, 25):
+#    print(j)
+#    landTest2 = land()
+#    score2 = landTest2.agent2()
+#    print(score2)
+#    scoreAgent2 += score2
+
+#print("Agent2: ", scoreAgent2/25)
+
+#landTest = land()
+#score1 = landTest.agent1()
+#print(score1)
+
+landTest2 = land()
+score2 = landTest2.agent2()
+print(score2)
