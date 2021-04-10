e469366ea7e6e8f7d3d98c00f967b7d2b19c851c
Fawaz Tahir <fttahir2301@gmail.com>
2021-04-10 00:58:02 -0400

    agent 1 and agent 2

--- Project_3/main.py
+++ Project_3/main.py
@@ -1,3 +1,4 @@
+from fraction import fraction
 import random
 import sys
 import csv
@@ -15,30 +16,154 @@ class cell:
         self.prob = prob
 
 
-class maze:
+class land:
     def __init__(self):
-        self.falseFlat = 0.1
-        self.falseHilly = 0.3
-        self.falseForested = 0.7
-        self.falseMaze = 0.9
-        dim = 50;
-        self.map = [ [ None for i in range(dim) ] for j in range(dim) ]
-        for i in range(0, dim):
-            for j in range(0, dim):
+        self.dim = 3;
+        self.FalseNegativeMap =  [ [ None for i in range(self.dim) ] for j in range(self.dim) ]
+        self.map = [ [ None for i in range(self.dim) ] for j in range(self.dim) ]
+        for i in range(0, self.dim):
+            for j in range(0, self.dim):
                 pt = point(i,j)
                 rand = random.randint(1, 4)
-                c = cell(pt, rand, 1/2500)
+                c = cell(pt, rand, 1/9)
+                if rand == 1:
+                    self.FalseNegativeMap[i][j] = 0.1
+                elif rand == 2:
+                    self.FalseNegativeMap[i][j] = 0.3
+                elif rand == 3:
+                    self.FalseNegativeMap[i][j] = 0.7
+                elif rand == 4:
+                    self.FalseNegativeMap[i][j] = 0.9
                 self.map[i][j] = c
+        self.Agent2Map =  self.map
 
-        randCol = random.randint(0, dim-1)
-        randRow = random.randint(0, dim-1)
+        randCol = random.randint(0, self.dim-1)
+        randRow = random.randint(0, self.dim-1)
         self.target = point(randCol, randRow)
 
+    def printLand(self):
+        for k in range(0, self.dim):
+            for m in range(0, self.dim):
+                print(self.map[k][m].state,' ', end = ' ')
+            print()
 
-    def printMaze
-        #for k in range(0, dim):
-        #    for m in range(0, dim):
-        #        print(self.map[k][m].state,' ', end = ' ')
+    def printProb(self):
+        for k in range(0, self.dim):
+            for m in range(0, self.dim):
+                print(self.map[k][m].prob,' ', end = ' ')
+            print()
+
+    def updateProb(self, col, row):
+        for i in range(0, self.dim):
+            for j in range(0, self.dim):
+                if i == col and j == row:
+                    continue
+                self.map[i][j].prob = (self.map[i][j].prob)/(self.map[col][row].prob*self.FalseNegativeMap[col][row]+(1-self.map[col][row].prob))
+        self.map[col][row].prob = (self.FalseNegativeMap[col][row]*self.map[col][row].prob)/(self.map[col][row].prob*self.FalseNegativeMap[col][row]+(1-self.map[col][row].prob))
+        self.printProb()
+        print()
+
+    def searchNext(self, currCol, currRow):
+        points = []
+        prob = self.map[0][0].prob
+        for i in range(0, self.dim):
+            for j in range(0, self.dim):
+                if self.map[i][j].prob>prob:
+                    points = []
+                    prob = self.map[i][j].prob
+                    pt = (i,j)
+                    points.append(pt)
+                elif self.map[i][j].prob == prob:
+                    pt = (i,j)
+                    points.append(pt)
+        col = points[0][0]
+        row= points[0][1]
+        distance = abs(currCol-col)+abs(currRow-row)
+
+        for point in points:
+            newDistance = abs(currCol-point[0])+abs(currRow-point[1])
+            if distance>newDistance:
+                distance = newDistance
+                col = point[0]
+                row = point[1]
+
+        return col, row, distance
+
+    def agent1(self):
+        self.printLand()
+        print("Target Row: ", self.target.col)
+        print("Target Col: ", self.target.row)
+        print()
+        currCol = random.randint(0, self.dim-1)
+        currRow = random.randint(0, self.dim-1)
+        distance = 0
+        search = 0
+        while True:
+            search +=1
+            print("Row: ", currCol)
+            print("Col: ", currRow)
+            if self.map[currCol][currRow].pt.col == self.target.col and self.map[currCol][currRow].pt.row == self.target.row:
+                if random.random() > self.FalseNegativeMap[currCol][currRow]:
+                    print('type: ', self.map[currCol][currRow].state)
+                    return distance+search
+            self.updateProb(currCol, currRow)
+            currCol, currRow, mDistance = self.searchNext(currCol, currRow)
 
 
-mazeTest = maze()
+            distance += mDistance
+
+    def Agent2Search(self, currCol, currRow):
+        points = []
+        print()
+        prob = self.map[0][0].prob*(1-self.FalseNegativeMap[0][0])
+        for i in range(0, self.dim):
+            for j in range(0, self.dim):
+                print((self.map[i][j].prob*(1-self.FalseNegativeMap[i][j])), ' ', end = ' ')
+                if (self.map[i][j].prob*(1-self.FalseNegativeMap[i][j]))>prob:
+                    points = []
+                    prob = self.map[i][j].prob*(1-self.FalseNegativeMap[i][j])
+                    pt = (i,j)
+                    points.append(pt)
+                elif (self.map[i][j].prob*(1-self.FalseNegativeMap[i][j])) == prob:
+                    pt = (i,j)
+                    points.append(pt)
+            print()
+        col = points[0][0]
+        row= points[0][1]
+        distance = abs(currCol-col)+abs(currRow-row)
+
+        for point in points:
+            newDistance = abs(currCol-point[0])+abs(currRow-point[1])
+            if distance>newDistance:
+                distance = newDistance
+                col = point[0]
+                row = point[1]
+
+        return col, row, distance
+
+    def agent2(self):
+        self.printLand()
+        print("Target Row: ", self.target.col)
+        print("Target Col: ", self.target.row)
+        print()
+        currCol = random.randint(0, self.dim-1)
+        currRow = random.randint(0, self.dim-1)
+        distance = 0
+        search = 0
+        while True:
+            search +=1
+            print("Row: ", currCol)
+            print("Col: ", currRow)
+            if self.map[currCol][currRow].pt.col == self.target.col and self.map[currCol][currRow].pt.row == self.target.row:
+                if random.random() > self.FalseNegativeMap[currCol][currRow]:
+                    print('type: ', self.map[currCol][currRow].state)
+                    return distance+search
+            self.updateProb(currCol, currRow)
+            currCol, currRow, mDistance = self.Agent2Search(currCol, currRow)
+
+
+            distance += mDistance
+
+landTest = land()
+score = landTest.agent2()
+print('score: ', score)
